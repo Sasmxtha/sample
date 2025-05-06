65170f146d1fcf0068441479b79cf429ff119a04
Sasmxtha <sasmxtha.s@gmail.com>
2025-05-06 22:35:40 +0530

    Update length.py

--- length.py
+++ length.py
@@ -115,4 +115,4 @@ def result_file(filename):
     return send_from_directory(RESULT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
